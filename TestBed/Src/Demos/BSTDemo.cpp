5bcd664cd9b815e0c01223384c43e1fa85d3cfaa
PC88 <32823980+PC88@users.noreply.github.com>
2019-06-24 19:52:23 +0100

    first visual elements added, further work still needed.

--- TestBed/Src/Demos/BSTDemo.cpp
+++ TestBed/Src/Demos/BSTDemo.cpp
@@ -8,8 +8,64 @@ BSTDemo::BSTDemo()
 
 	const b2Vec2 gravity = b2Vec2(0.0f, -10.0f);
 	m_world->SetGravity(gravity);
-}
 
+	/// DEFINE DYNAMIC BODIES ///
+	// define the elements that will be in the tree
+	{
+		for (int32 i = 0; i < e_BSTDemoElements; ++i)
+		{
+			b2PolygonShape shape;
+			shape.SetAsBox(1.5f, 1.5f);
+
+			b2BodyDef bd;
+			bd.type = b2_dynamicBody;
+			bd.position.Set(-30.0f, 27.0f);
+
+			m_elements[i] = m_world->CreateBody(&bd);
+
+			m_elements[i]->CreateFixture(&shape, 1.0f);
+
+			//m_elements[i]->SetLinearVelocity(b2Vec2(0.0f, -50.0f));
+		}
+	}
+
+	// define the dynamic gates which hold objects
+	{
+		for (int32 i = 0; i < e_BSTDemoElementGateJoints; ++i)
+		{
+			b2PolygonShape shape;
+			shape.SetAsBox(3.5f, 0.5f);
+
+			b2BodyDef bd;
+			bd.type = b2_dynamicBody;
+			bd.position.Set(-5.0f, 22.0f);
+
+			m_elementGates[i] = m_world->CreateBody(&bd);
+
+			m_elementGates[i]->CreateFixture(&shape, 1.0f);
+		}
+	}
+	/// DEFINE DYNAMIC BODIES ///
+
+	/// DEFINE STATIC BODIES ///
+	// define the static bridges which visualize the references to sub-nodes
+	{
+		for (int32 i = 0; i < e_BSTDemoElementBridges; ++i)
+		{
+			b2EdgeShape shape;
+			shape.Set(b2Vec2(-5.0f, 22.0f), b2Vec2(5.0f, 22.0f));
+
+			b2BodyDef bd;
+			bd.type = b2_staticBody;
+			bd.position.Set(0.0f, 10.0f);
+
+			m_elementBridges[i] = m_world->CreateBody(&bd);
+
+			m_elementBridges[i]->CreateFixture(&shape, 1.0f);
+		}
+	}
+	/// DEFINE STATIC BODIES ///
+}
 
 BSTDemo::~BSTDemo()
 {
--- TestBed/Src/Demos/BSTDemo.h
+++ TestBed/Src/Demos/BSTDemo.h
@@ -10,12 +10,13 @@ public:
 	BSTDemo();
 	virtual ~BSTDemo();
 
-	static const uint32 m_elementCount = 10; // number of elements in BST
+	static const uint32 m_elementCount = 12; // number of elements in BST
 
 	enum
 	{
 		e_BSTDemoElements = m_elementCount,
-		e_BSTDemoElementBridges = m_elementCount * 2
+		e_BSTDemoElementBridges = m_elementCount,
+		e_BSTDemoElementGateJoints = m_elementCount
 	};
 
 	void Box2DStart() override;
@@ -31,10 +32,20 @@ public:
 	uint32 m_velocityIterations; // usually 6
 	uint32 m_positionIterations; // usually 2
 
+	// TODO: create measurements, of how to space out gates, and bridges.
+
+
 	/// B2D elements used in demo ///
+
+	// Dynamic bodies -PC
 	b2Body* m_elements[e_BSTDemoElements];
 	b2Body* m_elementGates[e_BSTDemoElements]; // weighted gates
+
+	// Static bodies -PC
 	b2Body* m_elementBridges[e_BSTDemoElementBridges]; // static connecting bodies
+
+	// Joints -PC
+	b2RevoluteJoint* m_elementBridgeJoints[e_BSTDemoElementGateJoints]; // joints for the gates
 
 };
 
